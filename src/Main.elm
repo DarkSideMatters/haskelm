57b4dd6d635c5e97668189429b8e6ef06b7b89e6
DarkSideMatters <plamengetsov95@gmail.com>
2017-05-29 12:09:52 +0200

    modified/implemented elmchat

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,14 @@
 module Main exposing (main)
 
 -- import Html.App    as App
-import Html        exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import WebSocket
+import List
 
 -- main : Program Never
+main: Program Never Model Msg
 main =
   Html.program
      { init          = init
@@ -14,23 +17,41 @@ main =
      , subscriptions = subscriptions
      }
 
-type alias Model
-  = Int
+type alias Model =
+  { chatMessage : List String
+  , userMessage : String
+  , userName : String
+  }
 
 type Msg
-  = Receive String
-  | Send
+  = PostChatMessage
+  | UpdateUserMessage String
+  | NewChatMessage String
+  | LoginMessage String
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  ( Model [""] "" ""
+  , Cmd.none
+  )
+
+type alias ChatMessage =
+  { command: String
+  , content: String
+  }
 
 view : Model -> Html Msg
 view model =
   div []
-    [ p [] [ text <| "Pokes: " ++ toString model ]
-    , button [ onClick Send ] [ text "Poke others" ]
-    ]
+    [ div [] []
+    , input [ placeholder "message..."
+            , autofocus True
+            , value model.userMessage
+            , onInput UpdateUserMessage
+            ] []
+    , button [ onClick PostChatMessage ] [ text "Submit" ]
+    , div []  (List.map cMessage model.chatMessage)
+  ]
 
 wsUrl : String
 wsUrl = "ws://localhost:3000"
@@ -38,13 +59,27 @@ wsUrl = "ws://localhost:3000"
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Receive "poke" ->
-      (model + 1) ! []
-    Receive _ ->
-      model ! []
-    Send ->
-      model ! [ WebSocket.send wsUrl "poke" ]
+    PostChatMessage ->
+      let
+        message = model.userMessage
+      in
+        { model | userMessage = "" } ! [WebSocket.send "ws://localhost:3000/" message]
+
+    UpdateUserMessage message ->
+       { model | userMessage = message } ! []
+
+    NewChatMessage message ->
+       { model | chatMessage = message :: model.chatMessage } ! []
+
+    LoginMessage message ->
+       { model | userName = message } ! []
+
+
+cMessage : String -> Html msg
+cMessage message =
+  div [] [text message]
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  WebSocket.listen wsUrl Receive
+  WebSocket.listen wsUrl NewChatMessage
